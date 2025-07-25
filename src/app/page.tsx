a30584073a3df7d5649d3ec1aecc3c50afa3d2ea
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-25 22:43:47 +0600

    creating folder structures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Home = (props: Props) => {
   return (
     <div>
-      <p className='bg-red-500 text-6xl'>Test</p>
+      <p className='test text-6xl'>Test</p>
     </div>
   )
 }
