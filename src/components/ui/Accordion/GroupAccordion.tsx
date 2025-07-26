9e83db1b7a83296cf9d992ae1acbf8b6d3a33003
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-27 00:46:24 +0600

    hover added in accordion

--- src/components/ui/Accordion/GroupAccordion.tsx
+++ src/components/ui/Accordion/GroupAccordion.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { FaChevronUp,FaChevronDown } from "react-icons/fa";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
 type AccordionItem = {
   title: string;
@@ -38,17 +38,21 @@ const GroupAccordion: React.FC = () => {
         <div
           key={index}
           className={`py-2 ${
-            index !== data.length - 1 ? "border-b border-slate-200 border-dashed" : ""
+            index !== data.length - 1
+              ? "border-b border-slate-200 border-dashed"
+              : ""
           }`}
         >
           <button
             onClick={() => toggleAccordion(index)}
-            className="w-full flex items-center justify-between text-left"
+            className="w-full flex items-center justify-between text-left group"
           >
             <span className="max-w-[90%] font-medium md:text-base mx-lg:text-sm">
               {item.title}
             </span>
-            {openIndex === index ? <button><FaChevronUp/></button>:<button><FaChevronDown/></button>}
+            <span className="text-sm text-slate-300 group-hover:text-black transition duration-500">
+              {openIndex === index ? <FaChevronUp /> : <FaChevronDown />}
+            </span>
           </button>
           <div
             className={`transition-all duration-300 ease-in-out overflow-hidden ${
